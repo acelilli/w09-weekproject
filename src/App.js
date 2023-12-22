c13569cd2690be212924f5f4fb15312422a73a6a
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2023-12-22 12:10:44 +0100

    Impostazione + navbar full

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.css";
+import logo from "./logo.svg";
+import "./App.css";
+import NetflixNavbar from "./components/NetflixNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* NetflixNavbar */}
+      <NetflixNavbar />
+      {/* NetflixMain */}
+      {/* Netflix Footer */}
+    </div>
+  );
+}
+
+export default App;
